554f42cfdd80aa60ad9b0184fd69adc0668ec5bd
FajarFirdaus732 <35714197+FajarTheGGman@users.noreply.github.com>
2019-01-28 18:09:00 +0700

    Update banner.php

--- banner.php
+++ banner.php
@@ -10,14 +10,3 @@ IG : fajar_firdaus_7
 ";
 ?>
 
-
-// Lelaki yang mencuri dan wanita yang mencuri,potonglah tangan
-// keduanya (sebagai) pembalasan bagi apa yang mereka kerjakan dan
-// sebagai siksaan dari Allah.
-// Dan Allah Maha Perkasa lagi Maha Bijaksana.
-// Maka barangsiapa bertaubat (di antara pencuri-pencuri itu)
-// sesudah melakukan kejahatan itu dan memperbaiki diri,
-// maka sesungguhnya Allah menerima taubatnya.
-// Sesungguhnya Allah Maha Pengampun lagi Maha Penyayang.
-
-// (QS. Al-Maidah: 38-39)
